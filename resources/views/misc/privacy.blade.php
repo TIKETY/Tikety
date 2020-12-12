910887839c22c908f69e006c84ec80d5881acc74
kea137 <kea137@.github.com>
2020-12-12 20:56:54 +0300

    forgot this one

--- resources/views/misc/privacy.blade.php
+++ resources/views/misc/privacy.blade.php
@@ -134,9 +134,9 @@
                     </div>
                     <div class="card shadow">
                         <div class="card-header">
-                            <a class="collapsed card-link darkcolor" data-toggle="collapse" href="#collapsetwelve">{{ __('Children\'s Information') }}</a>
+                            <a class="collapsed card-link darkcolor" data-toggle="collapse" href="#collapsetwelvew">{{ __('Children\'s Information') }}</a>
                         </div>
-                        <div id="collapsetwelve" class="collapse" data-parent="#accordion">
+                        <div id="collapsetwelvew" class="collapse" data-parent="#accordion">
                             <div class="card-body">
                                 <p>{{ __('Another part of our priority is adding protection for children while using the internet. We encourage parents and guardians to observe, participate in, and/or monitor and guide their online activity. tikety does not knowingly collect any Personal Identifiable Information from children under the age of 13. If you think that your child provided this kind of information on our website, we strongly encourage you to contact us immediately and we will do our best efforts to promptly remove such information from our records.') }}</p>
                             </div>
