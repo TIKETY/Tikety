5cc10412ce36096da1305e19d34dd7432d39d8ec
kea137 <kea137@.github.com>
2021-01-23 13:02:28 +0300

    changed the 500 internal page to corespond to its message added the 413 page and their translation

--- resources/views/errors/413.blade.php
+++ resources/views/errors/413.blade.php
@@ -2,21 +2,21 @@
 
 @section('content')
 
-    <!--Error 500 SECTION-->
+    <!--Error 413 SECTION-->
     <section id="error" class="padding">
         <div class="container">
             <div class="row">
                 <div class="col-md-12 text-center">
                     <div class="error wow bounceIn" data-wow-delay="300ms">
-                        <h1>500</h1>
-                        <h2>500</h2>
+                        <h1>413</h1>
+                        <h2>413</h2>
                     </div>
-                    <p class="heading_space">{{ __('We are sorry, their is internal error.') }}</p>
+                    <p class="heading_space">{{ __('We are sorry, the page you want is not here.') }}</p>
                     <a href="{{ route('home', app()->getLocale()) }}" class="button btn-primary wow fadeIn mb-3 mb-sm-0" data-wow-delay="400ms">back to home</a>
                 </div>
             </div>
         </div>
     </section>
-    <!--Error 500 section end-->
+    <!--Error 413 section end-->
 
 @endsection
