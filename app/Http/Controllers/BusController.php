5cac240af5babaf43fa82a0b42d82eb86e748c92
kea137 <kea137@.github.com>
2021-01-24 21:09:18 +0300

    tinkering storage

--- app/Http/Controllers/BusController.php
+++ app/Http/Controllers/BusController.php
@@ -47,7 +47,7 @@ class BusController extends Controller
             'g-recaptcha-response'=>['required', new RecaptchaRule]
         ]);
 
-        Storage::disk('do')->putFile('buses', request()->file);
+        Storage::disk('do')->putFile('buses', $request->file('image_url'));
 
         $countries = new Countries;
         $states = $countries->whereNameCommon('Tanzania')->first()->hydrateStates()->states->pluck('name', 'postal')->toArray();
