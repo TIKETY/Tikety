4c3d62090120fcb46fde15cc8b76ba58562fcc73
kea137 <kea137@.github.com>
2021-01-30 13:05:12 +0300

    production testing 1

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -14,16 +14,19 @@ class RoleController extends Controller
     }
 
     public function makeRole($role){
-        dd('hello');
+
         $role1 = Role::find(1);
         $role2 = Role::find(2);
         $role3 = Role::find(3);
         if($role==='master'){
             auth()->user()->addRole($role1);
+            dd('hello1');
         } else if($role==='manager'){
             auth()->user()->addRole($role2);
+            dd('hello2');
         } else if($role==='customer') {
             auth()->user()->addRole($role3);
+            dd('hello3');
         }
 
         return redirect()->route('home', app()->getLocale());
