87c3c1580de15c423bdf304a73bd7ea451aa0bd6
kea137 <kea137@.github.com>
2021-01-30 13:17:36 +0300

    found the problem confident it will work, it was language parameters forgoten in the controller

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -13,20 +13,16 @@ class RoleController extends Controller
         return view('registration.role');
     }
 
-    public function makeRole($role){
-
+    public function makeRole($language, $role){
         $role1 = Role::find(1);
         $role2 = Role::find(2);
         $role3 = Role::find(3);
-        if($role==1){
+        if($role==='master'){
             auth()->user()->addRole($role1);
-            dd('hello1');
-        } else if($role==2){
+        } else if($role==='manager'){
             auth()->user()->addRole($role2);
-            dd('hello2');
-        } else if($role==3) {
+        } else if($role==='customer') {
             auth()->user()->addRole($role3);
-            dd('hello3');
         }
 
         return redirect()->route('home', app()->getLocale());
