e905bd720e747773214aba84cebbe4a0a20ddb9b
kea137 <kea137@.github.com>
2021-01-31 22:53:26 +0300

    test3

--- app/Http/Controllers/BusController.php
+++ app/Http/Controllers/BusController.php
@@ -90,8 +90,8 @@ class BusController extends Controller
     }
 
     public function show($language, Bus $bus){
-        $seats = (Arr::flatten($bus->seats()->where('bus_id', $bus->id)->where('user_id', NULL)->pluck('seat')))->sizeof();
-        dd($seats);
+        $seats = (Arr::flatten($bus->seats()->where('bus_id', $bus->id)->where('user_id', NULL)->pluck('seat')));
+        dd(sizeof($seats));
         return view('bus.busdetail', [
             'bus'=>$bus,
             'seats'=>$seats,
