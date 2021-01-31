947a8d483fe1be2c20c6ae3640469630e47254af
kea137 <kea137@.github.com>
2021-01-31 20:26:30 +0300

    changed the welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -197,17 +197,17 @@
                         <img src="{{ asset('image/bg-flat-header.jpg')}}" data-bgcolor="#e0e0e0" alt="" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="4" class="rev-slidebg" data-no-retina>
                         <!-- layers -->
                         <div class="overlay overlay-dark opacity-1"></div>
-                        <div class="tp-caption tp-resizeme col-md-12 col-sm-12" data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" data-y="['middle','middle','middle','middle']" data-voffset="['-70','-70','-70','-70']" data-whitespace="nowrap" data-responsive_offset="on"
+                        <div class="col-md-12 col-sm-12" data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" data-y="['middle','middle','middle','middle']" data-voffset="['-70','-70','-70','-70']" data-whitespace="nowrap" data-responsive_offset="on"
                             data-width="['none','none','none','none']" data-type="text" data-textalign="['center','center','center','center']" data-transform_idle="o:1;" data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:2000;e:Power4.easeInOut;"
                             data-transform_out="s:1000;e:Power2.easeInOut;s:1000;e:Power2.easeInOut;" data-start="1000" data-splitin="none" data-splitout="none">
                             <h2>{{ __('Simplifying your Ticketing processes') }}</h2>
                         </div>
-                        <div class="tp-caption tp-resizeme col-md-12 col-sm-12" data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" data-y="['middle','middle','middle','middle']" data-voffset="['12','12','12','12']" data-whitespace="nowrap" data-responsive_offset="on"
+                        <div class="col-md-12 col-sm-12" data-x="['center','center','center','center']" data-hoffset="['0','0','0','0']" data-y="['middle','middle','middle','middle']" data-voffset="['12','12','12','12']" data-whitespace="nowrap" data-responsive_offset="on"
                             data-width="['none','none','none','none']" data-type="text" data-textalign="['center','center','center','center']" data-fontsize="['24','24','20','20']" data-transform_idle="o:1;" data-transform_in="z:0;rX:0deg;rY:0;rZ:0;sX:2;sY:2;skX:0;skY:0;opacity:0;s:1000;e:Power2.easeOut;"
                             data-transform_out="s:1000;e:Power3.easeInOut;s:1000;e:Power3.easeInOut;" data-mask_in="x:0px;y:0px;s:inherit;e:inherit;" data-start="1500" data-splitin="none" data-splitout="none">
                             <p>{{ __('A Journey of thousand steps, begins with the first, and we aim to simplify it even better') }}</p>
                         </div>
-                        <div class="tp-caption tp-resizeme" data-x="['center','center','center','center']" data-hoffset="['20','20','20','20']" data-y="['middle','middle','middle','middle']" data-voffset="['90','90','90','90']" data-whitespace="nowrap" data-transform_idle="o:1;"
+                        <div class="col-md-12 col-sm-12" data-x="['center','center','center','center']" data-hoffset="['20','20','20','20']" data-y="['middle','middle','middle','middle']" data-voffset="['90','90','90','90']" data-whitespace="nowrap" data-transform_idle="o:1;"
                             data-transform_in="y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;s:1500;e:Power4.easeInOut;" data-transform_out="s:900;e:Power2.easeInOut;s:900;e:Power2.easeInOut;" data-start="1600" data-splitin="none" data-splitout="none"
                             data-responsive_offset="on">
                             <a class="transition-3 button btn-primary button-padding pagescroll font-13 mr-4" href="#our-feature">{{ __('Learn More') }}</a>
@@ -400,11 +400,11 @@
                 <div class="col-lg-3 col-md-6 col-sm-6">
                     <div class="footer_panel padding_bottom_half bottom20">
                         <a href="index.html" class="footer_logo bottom25"><img src="{{ asset('image/logo-transparent.png')}}" alt="MegaOne"></a>
-                        <p class="whitecolor bottom25">{{ __('Keep away from people who try to belittle your ambitions Small people always do that but the really great Friendly.') }}</p>
+                        <p class="whitecolor bottom25">{{ __('Easing your Ticketing Experience') }}</p>
                         <div class="d-table w-100 address-item whitecolor bottom25">
                             <span class="d-table-cell align-middle"><i class="fas fa-mobile-alt"></i></span>
                             <p class="d-table-cell align-middle bottom0">
-                                +255 654 660 654 <a class="d-block" href="mailto:web@support.com">web@support.com</a>
+                                +255 654 660 654 <a class="d-block" href="mailto:support@tikety.co.tz">support@tikety.co.tz</a>
                             </p>
                         </div>
                         <ul class="social-icons white wow fadeInUp" data-wow-delay="300ms">
@@ -432,11 +432,10 @@
                             <li><a href="{{ route('contact', app()->getLocale()) }}">{{ __('Contact') }}</a></li>
                             <li><a href="{{ route('faq', app()->getLocale()) }}">{{ __('FAQ') }}'s</a></li>
                             <li><a href="{{ route('privacy', app()->getLocale()) }}">{{ __('Privacy and Policy') }}'s</a></li>
-                            <li><a href="{{ route('faq', app()->getLocale()) }}">{{ __('Terms and Conditions of Use') }}'s</a></li>
                         </ul>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-6 col-sm-6">
+                {{-- <div class="col-lg-3 col-md-6 col-sm-6">
                     <div class="footer_panel padding_bottom_half bottom20">
                         <h3 class="whitecolor bottom25">{{ __('Business hours') }}</h3>
                         <p class="whitecolor bottom25">{{ __('Our support available to help you 24 hours a day, seven days week') }}</p>
@@ -447,7 +446,7 @@
                             <li><span>Calendar Events:</span> <span>24-Hour Shift</span></li>
                         </ul>
                     </div>
-                </div>
+                </div> --}}
             </div>
         </div>
     </footer>
