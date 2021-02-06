45ae293b6e0934d8ef130952e2098c9524340716
kea137 <kea137@.github.com>
2021-02-07 00:06:17 +0300

    video still iterated

--- resources/views/misc/about.blade.php
+++ resources/views/misc/about.blade.php
@@ -11,7 +11,7 @@
         <div class="row align-items-center">
             <div class="col-lg-6 col-md-6 padding_bottom_half">
                 <div class="image">
-                    <video width="640" height="480" autoplay loop>
+                    <video width="520" height="440" autoplay loop controls muted>
                         <source src="https://tikety.fra1.digitaloceanspaces.com/Tikety%20logo%20reveal.mp4" type="video/mp4">
                         Your browser does not support the video tag.
                     </video>
