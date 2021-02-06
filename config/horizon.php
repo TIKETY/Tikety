3ec78ae293ca6c049831445ff4d17213fb393c8d
kea137 <kea137@.github.com>
2021-02-07 00:25:55 +0300

    required redis and ready to try it in the work and horizon

--- config/horizon.php
+++ config/horizon.php
@@ -166,7 +166,7 @@ return [
 
     'defaults' => [
         'supervisor-1' => [
-            'connection' => 'database',
+            'connection' => 'redis',
             'queue' => ['default'],
             'balance' => 'auto',
             'maxProcesses' => 1,
