da75a76aa041089765cf051e5c3809534852e8c7
kea137 <kea137@.github.com>
2021-02-12 22:25:37 +0300

    updated telescope...

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -21,7 +21,7 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
         $this->hideSensitiveRequestDetails();
 
         Telescope::filter(function (IncomingEntry $entry) {
-            if ($this->app->environment('local')) {
+            if ($this->app->environment('production')) {
                 return true;
             }
 
@@ -40,8 +40,8 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
      */
     protected function hideSensitiveRequestDetails()
     {
-        if ($this->app->environment('local')) {
-            return;
+        if ($this->app->environment('production')) {
+            return true;
         }
 
         Telescope::hideRequestParameters(['_token']);
