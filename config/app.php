63fbee14a58b44741e11629a1fdffb24fa4ed9e9
kea137 <kea137@.github.com>
2021-02-12 20:02:52 +0300

    required telescope and made it a night mode, and validated a single user at the gate function... nothing more than that

--- config/app.php
+++ config/app.php
@@ -186,6 +186,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\HorizonServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        App\Providers\TelescopeServiceProvider::class,
 
     ],
 
