c19d1d599f1a8c615b4f88d848b4b5e205722951
kea137 <kea137@.github.com>
2021-05-06 16:19:47 +0300

    segment droped down untill further notice

--- resources/views/bus/busdetail.blade.php
+++ resources/views/bus/busdetail.blade.php
@@ -1,13 +1,7 @@
 @extends('layout.standard_app')
 
 @section('header_script')
-{{-- <x-analytics></x-analytics> --}}
-<script>
-    !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="Nbxx6jl4msBuPBq9b0oxNUpFxfw3N5lx";analytics.SNIPPET_VERSION="4.13.2";
-    analytics.load("Nbxx6jl4msBuPBq9b0oxNUpFxfw3N5lx");
-    analytics.page();
-    }}();
-</script>
+<x-analytics></x-analytics>
 <x-recaptcha>
     review
 </x-recaptcha>
@@ -735,7 +729,7 @@
                             @endfor
 
                         @can('isowner', $bus)
-                        <form id="payseat" action="{{ route('takeseat', ['language'=>app()->getLocale(), 'bus'=>$bus]) }}" method="post">
+                        <form action="{{ route('takeseat', ['language'=>app()->getLocale(), 'bus'=>$bus]) }}" method="post">
                             @csrf
                             <div class="col-md-12 col-sm-12">
                                 <input type="hidden" id="seats_id" name="seats_id">
@@ -743,7 +737,7 @@
                             </div>
                         </form>
                         @else
-                        <form id="payseat" action="{{ route('payseat', ['language'=>app()->getLocale(), 'bus'=>$bus]) }}" method="post">
+                        <form action="{{ route('payseat', ['language'=>app()->getLocale(), 'bus'=>$bus]) }}" method="post">
                             @csrf
                             <div class="col-md-12 col-sm-12">
                                 <input type="hidden" id="seats_id" name="seats_id">
@@ -838,27 +832,6 @@ function take(rate){
     document.getElementById("rate_star").value = rate_array[rate_array.length-1];
 }
 
-</script>
-
-<script>
-document.getElementById("payseat").onsubmit = function() {myFunction()};
-
-function myFunction() {
-    {
-        "type": "track",
-        "event": "User Registered",
-        "properties": {
-            "plan": "Pro Annual",
-            "accountType" : "Facebook"
-        }
-    }
-    !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","debug","page","once","off","on","addSourceMiddleware","addIntegrationMiddleware","setAnonymousId","addDestinationMiddleware"];analytics.factory=function(e){return function(){var t=Array.prototype.slice.call(arguments);t.unshift(e);analytics.push(t);return analytics}};for(var e=0;e<analytics.methods.length;e++){var key=analytics.methods[e];analytics[key]=analytics.factory(key)}analytics.load=function(key,e){var t=document.createElement("script");t.type="text/javascript";t.async=!0;t.src="https://cdn.segment.com/analytics.js/v1/" + key + "/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(t,n);analytics._loadOptions=e};analytics._writeKey="Nbxx6jl4msBuPBq9b0oxNUpFxfw3N5lx";analytics.SNIPPET_VERSION="4.13.2";
-    analytics.load("Nbxx6jl4msBuPBq9b0oxNUpFxfw3N5lx");
-    analytics.track("User Registered", {
-        plan: "Pro Annual",
-        accountType: "Facebook"
-});
-}
 </script>
 
 <style>
